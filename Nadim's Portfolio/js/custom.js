64686fe9c6965c062fff1cf0617699fa5c81e7db
Nadimh2218 <119486049+Nadimh2218@users.noreply.github.com>
2023-03-02 21:30:59 +0600

    Add files via upload

---
+++ Nadim's Portfolio/js/custom.js
@@ -0,0 +1,37 @@
+
+
+$(function(){
+
+
+
+
+    jQuery('[data-vbg]').youtube_background();
+
+    SkillProgress(".chart", ".chart_bar", ".label", 100, 1500);
+      
+  });
+
+
+  const bodyEl = document.body
+const hamburgerEl = document.querySelector('.hamburger')
+
+hamburgerEl.addEventListener('click', () => {
+	bodyEl.classList.toggle('active')
+
+
+
+
+  
+})
+
+
+new Typed('#typed',{
+  strings : ['Nadim Hasan','a Designer','a Developer','a Coder'],
+  typeSpeed : 80,
+  delaySpeed : 200,
+  loop : true
+});
+
+
+
+  
